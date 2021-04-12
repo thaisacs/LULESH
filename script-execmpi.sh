5b5a3dede98d44414b9622b5c268027ab8df756e
Thais Camacho <thaiscamachoo@gmail.com>
2021-04-12 12:05:49 -0300

    up

--- script-execmpi.sh
+++ script-execmpi.sh
@@ -2,13 +2,13 @@ DATASET=exp-results
 rm -rf $DATASET
 mkdir $DATASET
 
-sudo perf stat -o $DATASET/perf-asc-lulesh.out ./lulesh2.0 -i 100
-/usr/bin/time -o $DATASET/time-asc-lulesh.out -v ./lulesh2.0 -i 100
-ltrace -o $DATASET/mpi-asc-lulesh.out ./lulesh2.0 -i 100
+app=./lulesh2.0 -i 100
 
-cd $DATASET
-cat mpi-asc-lulesh.out | grep "MPI" > mpi2-asc-lulesh.out
-rm mpi-asc-lulesh.out
-mv mpi2-asc-lulesh.out mpi-asc-lulesh.out
+perf stat -o $DATASET/perf-asc-lulesh.out $app
+/usr/bin/time -o $DATASET/time-asc-lulesh.out -v $app
 
-#mpirun -n 2 ./lmp -var t 300 -echo screen -in lj/in.lj
+#ltrace -o $DATASET/mpi-asc-lulesh.out ./lulesh2.0 -i 100
+#cd $DATASET
+#cat mpi-asc-lulesh.out | grep "MPI" > mpi2-asc-lulesh.out
+#rm mpi-asc-lulesh.out
+#mv mpi2-asc-lulesh.out mpi-asc-lulesh.out
